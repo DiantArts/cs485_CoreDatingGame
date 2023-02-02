c903e3dd09d69adb45046a01de8467f9bcdced5a
DiantArts <DiantArts@gmail.com>
2023-02-02 15:38:03 -0800

    game kinda workable

--- main.py
+++ main.py
@@ -15,50 +15,60 @@ class TreeNode:
     # constructor
     def __init__(
         self, #: TreeNode
-        string: str
+        string: str,
+        isQuestion: bool = False
     ):
         self.text = string
         self.answers = [] # user input
         self.children = [] # IA answer (TreeNode type)
+        self.isQuestion = isQuestion
 
     # append an answer with its child node
     def append(
         self, #: TreeNode
         answer: str,
-        child #: TreeNode
+        child, #: TreeNode
     ):
         if not isinstance(child, TreeNode) and child is not None: # only accept TreeNode type
             raise ValueError("'child' object cannot be interpreted as a TreeNodes")
         self.answers.append(answer)
         self.children.append(child)
 
+    def isEmpty(
+        self, #TreeNode
+    ) -> bool:
+        return len(self.answers) == 0
+
     # print the tree
     def display(
         self, #: TreeNode
-        depth: int = 0
+        depth: int = 0,
     ):
-        print(self.asStr())
+        print(self.asStr(), end='')
 
     # convert to string
     def __repr__(
-        self
+        self, #: TreeNode
     ):
         return self.asStr()
 
     # convert to string
     def asStr(
         self, #: TreeNode
-        depth: int = 0
+        depth: int = 0,
     ):
         ret = (' ' * (depth * 4)) + self.text + '\n'
-        for (answer, child) in zip(self.answers, self.children):
-            ret += (' ' * (depth * 4)) + answer + '\n'
-            if child is not None:
-                ret += child.asStr(depth + 1)
+        if self.isQuestion:
+            ret += self.children[0].asStr(depth + 1)
+        else:
+            for (answer, child) in zip(self.answers, self.children):
+                ret += (' ' * (depth * 4)) + answer + '\n'
+                if child is not None:
+                    ret += child.asStr(depth + 1)
         return ret
 
 def printDebug(
-    string: str
+    string: str,
 ):
     if ENABLE_DEBUG:
         print(string)
@@ -69,7 +79,7 @@ def printDebug(
 
 # get the depth from the spaces at the begining of the line
 def getDepth(
-    line: str
+    line: str,
 ) -> int:
     return (len(line) - len(line.lstrip())) / TAB_SIZE
 
@@ -77,7 +87,7 @@ def getDepth(
 def extractAnswers(
     string: str,
     depth: int,
-    parent: TreeNode
+    parent: TreeNode,
 ) -> str:
     answers = []
     children = []
@@ -111,6 +121,7 @@ def extractAnswers(
             if child is not None:
                 parent.append(answer, child)
     else:
+        # answer without new text, game over if node reached
         parent.append(answer, None)
 
     # check if should continue searching
@@ -126,7 +137,7 @@ def extractAnswers(
 # recurssively called by extractAnswers()
 def generateTree(
     string: str,
-    depth: int = 0
+    depth: int = 0,
 ) -> (TreeNode, str):
 
     printDebug("\t\t\t===== diving <depth:" + str(depth) + ">")
@@ -141,8 +152,13 @@ def generateTree(
     printDebug("\t\t\t=====================> '" + string[:eolPos].lstrip(' ') + "' <depth:" + str(depth) + ">")
     string = string[eolPos + 1:]
 
-    # finding answers and recursively diving to generate subtrees
-    string = extractAnswers(string, depth, tree)
+    if tree.text.startswith("\\<question"):
+        tree.isQuestion = True
+        child, _ = generateTree(string, depth + 1)
+        tree.children.append(child)
+    else:
+        # finding answers and recursively diving to generate subtrees
+        string = extractAnswers(string, depth, tree)
 
     # node is complete, returning
     printDebug("\t\t\tdive done")
@@ -150,17 +166,127 @@ def generateTree(
 
 
 
+# =================================================== game tools
+
+def receiveTwillioInput(
+) -> str:
+    # TODO: reiceve with twillio here
+    return input("\n<Input>: ")
+
+def sendTwillioOutput(
+    phoneNumber: str,
+    text: str,
+):
+    # TODO: send with twillio here
+    print("<phone:" + phoneNumber + '>')
+    print(text)
+
+# parse the input, 0 means invalid input
+def parseInput(
+    string: str,
+) -> int:
+    if not string.isnumeric():
+        return 0
+    return int(string)
+
+
+
+# =================================================== Game definition
+
+class GameInstance:
+    def __init__(
+        self, #: GameInstance
+        gameTree: TreeNode
+    ):
+        self.tree = gameTree
+        self.instances = {}
+        self.name = "<UnknownName>"
+
+class Game:
+
+    def __init__(
+        self, #: Game
+        gameTree: TreeNode,
+    ):
+        self.tree = gameTree
+        self.instances = {}
+
+    def lauchInstance(
+        self, #: Game
+        phoneNumber: str,
+    ):
+        self.instances[phoneNumber] = GameInstance(self.tree)
+        sendTwillioOutput(phoneNumber, self.generateOutput(phoneNumber))
+
+    # generate output to send to twillio
+    def generateOutput(
+        self, #: Game
+        phoneNumber: str,
+    ) -> str:
+
+        # special questions
+        if self.instances[phoneNumber].tree.text.startswith("\\<question:"):
+            outputStr = self.instances[phoneNumber].tree.text.replace("\\<question:", '')
+            if outputStr.startswith("name>"):
+                outputStr = outputStr.replace("name>", '')
+            return outputStr
+
+        # default
+        outputStr = self.instances[phoneNumber].tree.text
+        for i in range(len(self.instances[phoneNumber].tree.answers)):
+            outputStr += '\n' + str(i + 1) + ': ' + self.instances[phoneNumber].tree.answers[i]
+        outputStr = outputStr.replace("\\<name>", self.instances[phoneNumber].name)
+        return outputStr
+
+    # False if game is over and instance is destroyed
+    def processInputOnInstance(
+        self, #: Game,
+        phoneNumber: str,
+        inputStr: str # input
+    ) -> bool:
+
+        inputNum = 0
+
+        # special questions
+        if self.instances[phoneNumber].tree.text.startswith("\\<question:"):
+            if self.instances[phoneNumber].tree.text.startswith("\\<question:name>"):
+                self.instances[phoneNumber].name = inputStr
+            inputNum = 1
+
+        else: # default
+            inputNum = parseInput(inputStr)
+            if inputNum == 0 or inputNum > len(self.instances[phoneNumber].tree.answers):
+                sendTwillioOutput(phoneNumber, "Invalid input")
+                return True
+
+        self.instances[phoneNumber].tree = self.instances[phoneNumber].tree.children[inputNum - 1]
+        return self.instances[phoneNumber].tree is not None
+
+
+
 # =================================================== main
 
 def main(
 ) -> int:
-    string = open("CoreGameStory.txt").read()
-    # string = open("SimpleExampleFile.txt").read()
+    string = open("Example.txt").read()
     printDebug(string)
     tree, _ = generateTree(string)
     if tree is None:
         raise ValueError("Invalid tree generation")
-    tree.display()
+    # tree.display()
+    # return 0
+
+    phoneNumber = "0123456789"
+
+    game = Game(tree)
+    game.lauchInstance(phoneNumber)
+
+    while True:
+        if not len(game.instances[phoneNumber].tree.children):
+            return False
+        if not game.processInputOnInstance(phoneNumber, receiveTwillioInput()):
+            break
+        sendTwillioOutput(phoneNumber, game.generateOutput(phoneNumber))
     return 0
 
 if __name__ == "__main__":
